2d6cbbe6135c90dc7712cfd06fac0fbb573e842f
voyagerdev26 <devs251898@gmail.com>
2026-07-05 16:03:48 +0530

    project completed

--- server/controllers/showController.js
+++ server/controllers/showController.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import Movie from "../models/Movie.js";
 import Show from "../models/Show.js";
+import { inngest } from "../inngest/index.js";
 
 //Api to get now playing movies from TMDB api
 export const getNowPlayingMovies = async (req,res)=>{
@@ -71,6 +72,13 @@ export const addShow = async (req,res)=>{
     if(showsToCreate.length>0){
       await Show.insertMany(showsToCreate);
     }
+
+    // trigger send new show notification inngest event
+    await inngest.send({
+      name:'app/show.added',
+      data:{movieTitle:movie.title}
+    })
+    
     res.json({success:true,message:"Show Added successfully."});
 
   } catch (error) {
